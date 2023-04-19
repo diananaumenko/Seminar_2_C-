a10d6717f730577e47d5e9ed574808f031bab36a
Diana Naumenko <diananaymenko@mail.ru>
2023-04-19 22:45:35 +0300

    Zadacha_10

---
+++ Zadacha_10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе 
+// показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+string text = Console.ReadLine();
+int chislo = Convert.ToInt32(text);
+
+if (chislo < 100 || chislo > 999)
+{
+    Console.WriteLine("Переписывайте, здесь не трехзначное число ");
+}
+else
+{
+    int middle = (chislo / 10) % 10;
+    Console.WriteLine("Средняя цифра числа " + middle);
+}
+
